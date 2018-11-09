d5c5a94d46e5ad8af9ef008027110cfb71fe8920
trungnguyen <quoctrunguit@gmail.com>
2018-11-09 20:49:14 +0700

    fix port

--- boot.py
+++ boot.py
@@ -1,5 +1,7 @@
 from stocker_server import app
 from stock_database.parse_csv import Migration
+import os
 
 migration = Migration()
-app.run(debug=False)
+port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port)
