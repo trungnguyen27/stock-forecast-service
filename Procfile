ce0247b8a0ec19998c942f62958dec10ab4b1321
trungnguyen <quoctrunguit@gmail.com>
2018-11-11 22:58:11 +0700

    integrate postgre sql

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn boot --preload
+release: python init_db.py
--- stock_database/parse_csv.py
+++ stock_database/parse_csv.py
@@ -8,43 +8,70 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy_utils import database_exists
 import pandas as pd
 import csv
+from flask_sqlalchemy import SQLAlchemy
+from stocker_server import app
+import os
+from flask_migrate import Migrate
+DB_URL = os.getenv("DATABASE_URL","postgres://pkfjujpoljriaq:7527e2b110024e147f74c8d05d61e6a793a504814adef500bc976c101f1ada94@ec2-54-225-115-234.compute-1.amazonaws.com:5432/d3umils42h7mt5")
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
+
+db = SQLAlchemy(app)
+db_session = db.session()
+migrate = Migrate(app, db)
+
 csv_path = configs['csv_path']
 sql_path = configs['sql_path']
 metastock_name = configs['metastock_name']
 
-Base = declarative_base()
+# class User(db.Model):
+#   id = db.Column(db.Integer, primary_key=True)
+#   name = db.Column(db.String(100))
+#   email = db.Column(db.String(100))
 
-class Price_History(Base):
-    __tablename__ = 'Price_History'
-    __table_args__ = {'sqlite_autoincrement':True}
+#   def __init__(self, name, email):
+#     self.name = name
+# self.email = email
 
-    id = Column(Integer, primary_key=True, nullable=False )
-    ticker = Column(String)
-    date = Column(Date)
-    opn = Column(Float)
-    hi = Column(Float)
-    lo = Column(Float)
-    close = Column(Float)
-    vol = Column(Float)
+
+class Price_History(db.Model):
+    # __tablename__ = 'Price_History'
+    # __table_args__ = {'sqlite_autoincrement':True}
+    id = db.Column(db.Integer, primary_key=True, nullable=False )
+    ticker = db.Column(db.String(100))
+    date = db.Column(db.Date)
+    opn = db.Column(db.Float)
+    hi = db.Column(db.Float)
+    lo = db.Column(db.Float)
+    close = db.Column(db.Float)
+    vol = db.Column(db.Float)
 
 class Migration():
     def __init__(self):
-        if not database_exists('sqlite:///%s' %sql_path):
-            # data = pd.read_csv('%s/%s.csv' %(csv_path, metastock_name), parse_dates=[1], usecols = [0,1,2,3,4,5,6])
-            # #data=data[data.columns[:7]]
-            # print(data.describe())
-            # data.columns= ["Ticker","Date","Open", "High", "Low", "Close", "Volume"]
-            # data['Date'] = pd.to_datetime(data['Date']).apply(lambda x : x.date())
-            # print('dataframe loaded')
-            self.connect_database()
+        
+        self.connect_database()
+        try:
+            db.create_all()
+        except Exception as ex:
+            print(ex)
+        finally:
             self.load_csv()
-            #self.data = data
-            #self.init_database()
-        else:
-            self.connect_database()
+        # if not database_exists(postgre_url):
+        #     print('NOT EXISTS')
+        #     # data = pd.read_csv('%s/%s.csv' %(csv_path, metastock_name), parse_dates=[1], usecols = [0,1,2,3,4,5,6])
+        #     # #data=data[data.columns[:7]]
+        #     # print(data.describe())
+        #     # data.columns= ["Ticker","Date","Open", "High", "Low", "Close", "Volume"]
+        #     # data['Date'] = pd.to_datetime(data['Date']).apply(lambda x : x.date())
+        #     # print('dataframe loaded')
+        #     self.connect_database()
+        #     self.load_csv()
+        #     #self.data = data
+        #     #self.init_database()
+        # else:
+            
 
     def load_csv(self):
-        chunksize = 10000
+        chunksize = 1
         try:
             for chunk in pd.read_csv('%s/%s.csv' %(csv_path, metastock_name), chunksize= chunksize, parse_dates=[1], usecols = [0,1,2,3,4,5,6]):
                 chunk.columns= ["Ticker","Date","Open", "High", "Low", "Close", "Volume"]
@@ -56,17 +83,18 @@ class Migration():
             print('done reading csv')
 
     def connect_database(self):
+        
          # create the database
-        print('Connecting to database')
-        engine = create_engine('sqlite:///%s' %sql_path)
-        Base.metadata.create_all(engine)
+        # print('Connecting to database')
+        # engine = create_engine(postgre_url)
+        # Base.metadata.create_all(engine)
 
-        #create a session
-        session = sessionmaker()
-        session.configure(bind = engine)
-        s = session()
-        self.egnine = engine
-        self.session = s
+        # #create a session
+        # session = sessionmaker()
+        # session.configure(bind = engine)
+        # # s = session()
+        # self.egnine = engine
+        self.session = db_session
 
     def save_to_database(self, data):
         t = time()
@@ -84,6 +112,7 @@ class Migration():
                 })
                 #add all the records
                 s.add(record) 
+                print(index)
             s.commit()
             print("nana")
         except Exception as e:
