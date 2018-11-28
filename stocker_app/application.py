dbccdc4e536b08d3911a4fc7d249cc8ac1df45cf
trungnguyen <quoctrunguit@gmail.com>
2018-11-28 13:44:59 +0700

    update redis configs

--- stocker_app/application.py
+++ stocker_app/application.py
@@ -12,8 +12,8 @@ from stocker_app.config.setting import configs
 app =  Flask(__name__)
 
 app.config.update(
-    CELERY_BROKER_URL='redis://@127.0.0.1:6379/0',
-    CELERY_RESULT_BACKEND='redis://@127.0.0.1:6379/0'
+    CELERY_BROKER_URL='redis://@redis:6379/0',
+    CELERY_RESULT_BACKEND='redis://@redis:6379/0'
 )
 
 postgre_connection_string=configs['postgre_connection_string']
