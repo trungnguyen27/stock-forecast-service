106ef0a386069f27b563a7862c2c8fd7cb0fd0e3
trungnguyen <quoctrunguit@gmail.com>
2018-12-03 11:51:42 +0700

    add sql test file

--- test_sql.py
+++ test_sql.py
@@ -1,3 +1,25 @@
 from stocker_app.stock_database.migration.parse_csv import Migration
-m = Migration()
-m.migrate()
+from stocker_app.stock_database.DOA import DOA
+from stocker_app.stock_database.financial_data import FinancialData
+from stocker_app.stocker_logic.stock_model import SModel
+
+
+ticker = 'VNM'
+lags = [10]
+prior = 0.1
+stock = FinancialData(ticker)
+smodel = SModel(stock)
+mas = stock.get_moving_averages(lags=lags)
+smodel.intialize_model_parameters(changepoint_prior_scale=prior)
+predictions = smodel.predict(training_sets = mas, lags =lags,  days = 30)
+
+
+def write(df, ticker, start_date, prior, lag ):
+    doa = DOA()
+    data = doa.save_prediction(ticker = ticker, start_date = start_date, prior = prior, lag = lag, prediction_df = df)
+    print(data)
+
+for index, (key, result) in enumerate(predictions.items()):
+    trimmed = result[['ds', 'yhat', 'y', 'yhat_upper', 'yhat_lower']]
+    result.describe()
+    print(trimmed.head())
