ef0cfbbc45bbe1c8969d14f1cb842c8143c30b61
trungnguyen <quoctrunguit@gmail.com>
2018-12-06 11:57:35 +0700

    [Utils] update hashing function

--- stocker_app/utils/database_utils.py
+++ stocker_app/utils/database_utils.py
@@ -1,8 +1,11 @@
 import hashlib
 import json
+
 def get_prediction_model_hash(model_params):
     sha = hashlib.sha256()
-    jsonobj = json.dumps(model_params, sort_keys=True)
+    jsonobj = json.dumps(model_params, sort_keys=True, default=str)
     print('HASHING: %s' %jsonobj)
     sha.update(jsonobj.encode('utf-8'))
-    return sha.hexdigest()
+    hashed = sha.hexdigest()
+    print('HASHED: %s' %hashed)
+    return hashed
